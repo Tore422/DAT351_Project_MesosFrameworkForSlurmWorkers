89409cf4b0e654d3dbe03c1e9046bcc8ff87cb78
Tore Berven <tore.berven@student.uib.no>
2021-11-24 14:11:40 +0100

    Made a class for representing a job
    Job has:
    * A state, an Id, resource requirements (CPU, Memory),
      a command to be executed, and a set number of retry attempts.
    * A method for converting the Job into a Mesos task.
    * Methods for updating the state and number of retries of
      the Job.
    * A toString implementation.

---
+++ src/main/java/job/Job.java
@@ -0,0 +1,95 @@
+package job;
+
+import org.apache.mesos.Protos;
+
+import java.util.UUID;
+
+enum JobState {
+    PENDING,
+    STAGING,
+    RUNNING,
+    SUCCESSFUL,
+    FAILED
+}
+
+public class Job {
+
+    private final String id;
+    private double cpus;
+    private double memory;
+    private String command;
+    private int numberOfRetries;
+    private JobState state;
+
+    private static final int MAX_NUMBER_OF_RETRIES = 5;
+
+    public Job() {
+        this.id = UUID.randomUUID().toString();
+        this.numberOfRetries = MAX_NUMBER_OF_RETRIES;
+        this.state = JobState.PENDING;
+    }
+
+    public Job(double cpus, double memory, String command) {
+        this.id = UUID.randomUUID().toString();
+        this.cpus = cpus;
+        this.memory = memory;
+        this.command = command;
+        this.numberOfRetries = MAX_NUMBER_OF_RETRIES;
+        this.state = JobState.PENDING;
+    }
+
+    public Protos.TaskInfo makeJobIntoTask(Protos.SlaveID targetSlaveID) {
+        UUID uuid = UUID.randomUUID();
+        Protos.TaskID taskID = Protos.TaskID.newBuilder()
+                .setValue(uuid.toString())
+                .build();
+        return Protos.TaskInfo.newBuilder()
+                .setName("task " + taskID.getValue())
+                .setTaskId(taskID)
+                .addResources(Protos.Resource.newBuilder()
+                        .setName("cpus")
+                        .setType(Protos.Value.Type.SCALAR)
+                        .setScalar(Protos.Value.Scalar.newBuilder()
+                                .setValue(this.cpus)))
+                .addResources(Protos.Resource.newBuilder()
+                        .setName("memory")
+                        .setType(Protos.Value.Type.SCALAR)
+                        .setScalar(Protos.Value.Scalar.newBuilder()
+                                .setValue(this.memory)))
+                .setSlaveId(targetSlaveID)
+                .setCommand(Protos.CommandInfo.newBuilder()
+                        .setValue(this.command))
+                .build();
+    }
+
+    public void launch() {
+        state = JobState.STAGING;
+    }
+
+    public void started() {
+        state = JobState.RUNNING;
+    }
+
+    public void succeeded() {
+        state = JobState.SUCCESSFUL;
+    }
+
+    public void failed() {
+        if (numberOfRetries <= 0) {
+            state = JobState.FAILED;
+        } else {
+            numberOfRetries--;
+            state = JobState.PENDING;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Job = {Id: " + this.id
+                + " , CPU: " + this.cpus
+                + " , Memory: " + this.memory
+                + " , Number of retries: " + this.numberOfRetries
+                + " , State: " + this.state
+                + " , Command: '" + this.command + "' }";
+    }
+}
